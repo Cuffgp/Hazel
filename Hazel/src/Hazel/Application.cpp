035517db737b4f96f4c3c231a0ddd7cf42ad865e
Keith Cuff <kcuff@ualberta.ca>
2019-09-25 01:16:34 -0600

    Windowing system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,13 +1,14 @@
+#include "hzpch.h"
 #include "Application.h"
 
 #include "Hazel/Events/ApplicationEvent.h"
-#include "Hazel/Log.h"
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
 
 	Hazel::Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Hazel::Application::~Application()
@@ -17,10 +18,12 @@ namespace Hazel {
 
 	void Hazel::Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		HZ_TRACE(e);
-
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 
 }
