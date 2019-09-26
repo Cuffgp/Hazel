b5d93a4d9344f9328d46938ff80e897271fb3fb0
Keith Cuff <kcuff@ualberta.ca>
2019-09-25 23:25:36 -0600

    Window Events

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -17,7 +17,7 @@ namespace Hazel {
 		inline unsigned int GetWidth() const override { return m_Data.Width; }
 		inline unsigned int GetHeight() const override { return m_Data.Height; }
 
-		// Windows attrubutes
+		// Windows attributes
 		inline void SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
 		void SetVSynch(bool enabled) override;
 		bool IsVSynch() const override;
